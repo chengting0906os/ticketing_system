ff9acf0e23d22f9f4e2afac1615dcd1c28bb6ac8
ChengTing <aaronzhan0906@gmail.com>
2025-09-08 13:49:12 +0800

    chore: add some knowledge of fastapi-user

--- src/user/use_case/manager.py
+++ src/user/use_case/manager.py
@@ -1,30 +1,42 @@
 """User manager for FastAPI Users integration."""
+# https://fastapi-users.github.io/fastapi-users/10.1/configuration/user-manager/
 
 from typing import Optional
 
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin
 
-from src.shared.logging.loguru_io import Logger
 from src.shared.config import settings
+from src.shared.logging.loguru_io import Logger
 from src.user.domain.user_model import User
 from src.user.infra.get_user_db import get_user_db
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    """User manager for FastAPI Users."""
-    
     reset_password_token_secret = settings.RESET_PASSWORD_TOKEN_SECRET
     verification_token_secret = settings.VERIFICATION_TOKEN_SECRET
     
 
     @Logger.io
     async def on_after_register(self, user: User, request: Optional[Request] = None):
-        """Actions after user registration."""
-        print(f'User {user.email} has registered.')
-    
-    # No need to override create() anymore - parent class handles email uniqueness
+        """Hook called after successful user registration.
+
+        Note:
+            This method can be used to:
+            - Send welcome emails
+            - Create initial user settings
+            - Trigger webhooks or notifications
+        """
+        pass
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        pass
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        pass
 
 async def get_user_manager(user_db=Depends(get_user_db)):
-    """Get user manager."""
     yield UserManager(user_db=user_db)
