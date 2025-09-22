c71e3e403b4438ca923fc834b24d3dd288f2f9a6
ChengTing <chengting0906os@gmail.com>
2025-09-22 12:20:20 +0800

    feat(booking): add relationship

--- src/booking/infra/booking_model.py
+++ src/booking/infra/booking_model.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 
 from sqlalchemy import ARRAY, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from src.shared.config.db_setting import Base
+
+
+if TYPE_CHECKING:
+    from src.event_ticketing.infra.event_model import EventModel
+    from src.user.domain.user_model import User
 
 
 class BookingModel(Base):
@@ -26,3 +31,8 @@ class BookingModel(Base):
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
     )
     paid_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable=True)
+
+    # Relationships
+    buyer: Mapped['User'] = relationship('User', foreign_keys=[buyer_id], lazy='select')
+    seller: Mapped['User'] = relationship('User', foreign_keys=[seller_id], lazy='select')
+    event: Mapped['EventModel'] = relationship('EventModel', foreign_keys=[event_id], lazy='select')
