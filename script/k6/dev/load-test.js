3c713770bfa0f839e4057f229b2495a087051ea5
ChengTing <chengting0906os@gmail.com>
2026-01-01 18:54:00 +0800

    test: extend loadtest time to 1m

--- script/k6/dev/load-test.js
+++ script/k6/dev/load-test.js
@@ -9,11 +9,11 @@ export const options = {
       executor: 'ramping-arrival-rate',
       startRate: 500,
       timeUnit: '1s',
-      preAllocatedVUs: 1500,
-      maxVUs: 1500,
+      preAllocatedVUs: 1400,
+      maxVUs: 1400,
       stages: [
         { target: 1000, duration: '5s' },    // ~3K
-        { target: 1500, duration: '30s' },  // ~65K
+        { target: 1400, duration: '50s' },  // ~65K
         { target: 1000, duration: '5s' },    // ~5K
       ],
     },
